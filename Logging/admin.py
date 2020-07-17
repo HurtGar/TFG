ff919bcb02c4acf1ad166ed8fc9fb81dcf9f52ad
HurtGar <adrian91hg@gmail.com>
2020-07-17 14:29:20 +0200

    Tablas añadidas a admin

--- Logging/admin.py
+++ Logging/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from Logging.models import Tarea,HistorialModificacionTarea,Usuario, Proyecto, Prioridad, RegistroEntradaUsuario,Tareasusuario,Estado,Rol,Bloque
 
 # Register your models here.
+
+admin.site.register(Tarea)
+admin.site.register(HistorialModificacionTarea)
+admin.site.register(Usuario)
+admin.site.register(Proyecto)
+admin.site.register(Prioridad)
+admin.site.register(RegistroEntradaUsuario)
+admin.site.register(Tareasusuario)
+admin.site.register(Estado)
+admin.site.register(Rol)
+admin.site.register(Bloque)
